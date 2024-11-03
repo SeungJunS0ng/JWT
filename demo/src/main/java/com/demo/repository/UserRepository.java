6e2daf81716463375fb9fc11478108a56173d54f
seung jun <john091122@naver.com>
2024-11-03 18:46:07 +0900

    Initial commit

---
+++ demo/src/main/java/com/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.demo.repository;
+
+import com.demo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
+}
